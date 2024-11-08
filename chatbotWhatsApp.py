0fe6ba07f5673b350aee0c0a579c24f63ed3f525
Luis Cortazar <luisjcortazarm@outlook.com>
2024-11-07 19:06:44 -0700

    Level 19 of the course sending and receiving messages in WA

--- chatbotWhatsApp.py
+++ chatbotWhatsApp.py
@@ -12,6 +12,9 @@ driver = webdriver.Chrome(options=options)
 driver.set_window_position(0,0)
 driver.set_window_size(1024,720)
 
+#implementar tiempo de espera
+driver.implicitly_wait(3600)
+
 #Solicitar url especifica
 driver.get("https://web.whatsapp.com")
 
@@ -25,16 +28,21 @@ while (True):
     #//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]
 
     #Recuperamos nuevos mensajes
-    new_message =driver.find_element(By.XPATH,'//*[@id="pane-side"]/descendant::span[contains(@aria-label\'no leidos\')]')
+    new_message = driver.find_element(By.XPATH, '//*[@id="pane-side"]/descendant::span[contains(@aria-label,\'no leído\')]')
     new_message.click()
     
     #Obtener los mensajes del usuario
-    last_messages =driver.find_elements(By.XPATH,'//*[@id="main"]/descendant::div[@role=\'row\']')
+    last_messages = driver.find_elements(By.XPATH, '//*[@id="main"]/descendant::div[@role=\'row\']')
     last_message = last_messages[-1]
     
     #Mostrar mensaje seleccionado
     last_message_text = last_message.find_element(By.XPATH, '//div/div/div[1]/div[1]/div[1]/div/div[1]/div/span[1]/span')
     print(f'user message:{last_message.text}')
    
-    
+    #Notificar al usuario
+    print('sending message to language service')
+    api_message = "Mesa de respuesta desde la API"
+    message_element = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]')
+    message_element.send_keys(f'{api_message}{Keys.ENTER}')
+    message_element.send_keys(Keys.ESCAPE)
 
